0112e38eb954cbd9da85615e9cc32ae8d6d04acd
nihclil <oxx921@gmail.com>
2024-11-03 05:18:50 +0800

    feat: add stateless layout account-deletion page

---
+++ packages/mesh/app/account-deletion/layout.tsx
@@ -0,0 +1,10 @@
+'use client'
+import LayoutTemplate from '@/components/layout-template'
+
+export default function AccountDeletionLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <LayoutTemplate type="stateless">{children}</LayoutTemplate>
+}
