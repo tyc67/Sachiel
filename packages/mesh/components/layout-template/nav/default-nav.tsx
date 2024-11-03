0fc1392562a5eaa32fbfee3f3f8fbf9915189fad
nihclil <oxx921@gmail.com>
2024-11-03 22:43:02 +0800

    refactor: uncomment some code

--- packages/mesh/components/layout-template/nav/default-nav.tsx
+++ packages/mesh/components/layout-template/nav/default-nav.tsx
@@ -8,7 +8,6 @@ import InteractiveIcon from '@/components/interactive-icon'
 import Avatar from '@/components/story-card/avatar'
 import { MOBILE_NAV_ICONS, NON_MOBILE_NAV_ICONS } from '@/constants/layout'
 import { isUserLoggedIn, useUser } from '@/context/user'
-import { logout } from '@/utils/logout'
 import { matchPath } from '@/utils/nav-button'
 
 type IconInfo = {
@@ -102,17 +101,13 @@ const NonMobileNav = ({
         {/* bottom (third) part */}
         {isLoggedIn && (
           <div className="flex flex-col border-t py-6">
-            {/* TODO: uncomment after setting page is implemented */}
-            {/* {NON_MOBILE_NAV_ICONS.third.map((iconInfo) => (
-            <NonMobileNavIcon
-              key={iconInfo.text}
-              isOn={matchPath(iconInfo.href, path)}
-              iconInfo={iconInfo}
-            />
-          ))} */}
-            <button className="button text-primary-500" onClick={logout}>
-              登出
-            </button>
+            {NON_MOBILE_NAV_ICONS.third.map((iconInfo) => (
+              <NonMobileNavIcon
+                key={iconInfo.text}
+                isOn={matchPath(iconInfo.href, path)}
+                iconInfo={iconInfo}
+              />
+            ))}
           </div>
         )}
       </div>
