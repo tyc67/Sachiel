dd96910c80739448b06f1518e4b91e3cd6f1ad37
caesar <caesar.lee@mirrormedia.mg>
2024-11-04 20:43:10 +0800

    refactor(mesh): show dialog before leaving

--- packages/mesh/app/collection/(mutate)/_components/desktop/desktop-go-back-button.tsx
+++ packages/mesh/app/collection/(mutate)/_components/desktop/desktop-go-back-button.tsx
@@ -1,19 +1,43 @@
 import { useRouter } from 'next/navigation'
+import { useRef } from 'react'
 
+import Dialog from '@/components/dialog'
 import GoBackButton from '@/components/navigation/go-back-button'
 import { useEditCollection } from '@/context/edit-collection'
 
 export default function DesktopGoBackButton() {
+  const dialogRef = useRef<HTMLDialogElement>(null)
   const router = useRouter()
   const { step, setStep } = useEditCollection()
 
   const onGoBackClicked = () => {
     if (step === 0) {
-      router.back()
+      dialogRef.current?.showModal()
     } else {
       setStep(step - 1)
     }
   }
 
-  return <GoBackButton customAction={onGoBackClicked} />
+  return (
+    <>
+      <GoBackButton customAction={onGoBackClicked} />
+      <Dialog
+        ref={dialogRef}
+        title="確認要退出編輯？"
+        description="系統不會儲存您所做的變更"
+        primayAction={{
+          text: '繼續編輯',
+          action: () => {
+            dialogRef.current?.close()
+          },
+        }}
+        secondaryAction={{
+          text: '退出',
+          action: () => {
+            router.back()
+          },
+        }}
+      />
+    </>
+  )
 }
---
+++ packages/mesh/components/dialog.tsx
@@ -0,0 +1,49 @@
+import type { ForwardedRef } from 'react'
+import { forwardRef } from 'react'
+
+import Button from './button'
+
+type DialogAction = {
+  text: string
+  action: () => void
+}
+
+type DialogProps = {
+  title?: string
+  description?: string
+  primayAction: DialogAction
+  secondaryAction: DialogAction
+}
+
+export default forwardRef(function Dialog(
+  { title, description, primayAction, secondaryAction }: DialogProps,
+  ref: ForwardedRef<HTMLDialogElement>
+) {
+  return (
+    <dialog
+      ref={ref}
+      className="w-[278px] rounded-lg border px-5 py-4 shadow-light-box lg:w-[400px]"
+    >
+      <div className="flex flex-col gap-1">
+        {title && <h3 className="title-2 text-primary-700">{title}</h3>}
+        {description && (
+          <p className="body-3 text-primary-700">{description}</p>
+        )}
+      </div>
+      <div className="flex flex-row items-center justify-end gap-3 pt-5">
+        <Button
+          size="sm"
+          color="transparent-blue"
+          text={secondaryAction.text}
+          onClick={secondaryAction.action}
+        />
+        <Button
+          size="sm"
+          color="custom-blue"
+          text={primayAction.text}
+          onClick={primayAction.action}
+        />
+      </div>
+    </dialog>
+  )
+})
