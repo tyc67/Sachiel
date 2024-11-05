66d3042a5f6f5b73a77f6e718fe4d4365d918699
nihclil <oxx921@gmail.com>
2024-11-05 16:09:52 +0800

    refactor: comment about us

--- packages/mesh/constants/layout.ts
+++ packages/mesh/constants/layout.ts
@@ -27,7 +27,8 @@ const DOWNLOAD_APP_LINKS = [
 
 // TODO: update path
 const FOOTER_NAV_LINKS = [
-  { text: '關於我們', href: '/' },
+  // TODO: Uncomment it when page is complete
+  // { text: '關於我們', href: '/' },
   { text: '聯絡我們', href: '/' },
   { text: '隱私政策', href: '/' },
   { text: '服務條款', href: '/' },
