5b79297160961f49e2f3182a6d97017b9bda0a09
YuCheTeng <114332515+tyc67@users.noreply.github.com>
2024-12-19 22:50:36 +0800

    refactor: layout-header

--- packages/mesh/components/layout-template/header/collection-header.tsx
+++ packages/mesh/components/layout-template/header/collection-header.tsx
@@ -2,21 +2,18 @@
 
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 import Button from '@/components/button'
 import Icon from '@/components/icon'
+import NotificationWrapper from '@/components/notification-wrapper'
 import { LOGO_ICONS } from '@/constants/layout'
 import { isUserLoggedIn, useUser } from '@/context/user'
 
 export default function CollectionHeader() {
-  // TODO: implement notification system
-  const [showNotification, setShowNotification] = useState(false)
   const router = useRouter()
   const { user } = useUser()
   const isLoggedIn = isUserLoggedIn(user)
-  const newNotification = true
 
   return (
     <header className="fixed inset-x-0 top-0 z-layout h-[theme(height.header.default)] border-b bg-white sm:h-[theme(height.header.sm)]">
@@ -49,36 +46,7 @@ export default function CollectionHeader() {
             </Link>
           </HeaderIconWrapper>
           {isLoggedIn ? (
-            newNotification ? (
-              // TODO: replace with correct path
-              <div>
-                {/* TODO: uncomment the code after notification has been implemented  */}
-                {/* <HeaderIconWrapper
-                  onClick={async () => {
-                    // TODO: show notification panel
-                    setShowNotification(!showNotification)
-                  }}
-                  className={
-                    showNotification ? 'rounded-[50%] bg-primary-100' : ''
-                  }
-                >
-                  <Icon size="2xl" iconName="icon-notifications-new" />
-                </HeaderIconWrapper> */}
-              </div>
-            ) : (
-              // TODO: replace with correct path
-              <HeaderIconWrapper
-                onClick={() => {
-                  // TODO: show notification panel
-                  setShowNotification(!showNotification)
-                }}
-                className={
-                  showNotification ? 'rounded-[50%] bg-primary-100' : ''
-                }
-              >
-                <Icon size="2xl" iconName="icon-notifications" />
-              </HeaderIconWrapper>
-            )
+            <NotificationWrapper />
           ) : (
             <div className="mx-3 my-1 flex items-center">
               <Button
